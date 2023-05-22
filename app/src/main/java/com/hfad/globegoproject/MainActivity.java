54be953c5eaa956dd9ed939ddd084a583987707f
JJSolvang <97262570+JJSolvang@users.noreply.github.com>
2023-05-21 19:31:06 -0700

    Implemented Basic Navigation Based on Wireframes

--- app/src/main/java/com/hfad/globegoproject/MainActivity.java
+++ app/src/main/java/com/hfad/globegoproject/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById((R.id.button_Continue)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),NavigationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
