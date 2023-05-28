201826682f7b920114c38ad12969619b1c3eaebf
Viktoria <viktoriia_morozova@outlook.com>
2023-05-27 21:04:26 -0700

    Geo Step Counter, work in progress

---
+++ app/src/main/java/com/hfad/globegoproject/GeoStepCount.java
@@ -0,0 +1,45 @@
+package com.hfad.globegoproject;
+
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class GeoStepCount {
+    public GeoStepCount(Location where, float steps) {
+        lattitude = where.getLatitude();
+        longitude = where.getLongitude();
+        stepsCount = (int)steps;
+        timestamp = where.getTime();
+        date = Date.from(Instant.ofEpochSecond(timestamp));
+    }
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getStepsCount() {
+        return stepsCount;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    private double lattitude;
+    private double longitude;
+    private int stepsCount;
+    private long timestamp;
+    @NonNull
+    private final Date date;
+}
